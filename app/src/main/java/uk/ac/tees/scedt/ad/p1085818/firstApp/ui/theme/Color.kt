e9cbbe9c04cf1993b45a3b016392a6f0e6a2689d
p1085818 <p1085818@live.tees.ac.uk>
2023-01-30 10:37:41 +0000

    first commit

---
+++ app/src/main/java/uk/ac/tees/scedt/ad/p1085818/firstApp/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package uk.ac.tees.scedt.ad.p1085818.firstApp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
---
+++ app/src/main/java/uk/ac/tees/scedt/ad/p1085818/firstApp/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package uk.ac.tees.scedt.ad.p1085818.firstApp.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(4.dp),
+        large = RoundedCornerShape(0.dp)
+)
